d87bb7735735a0a9dd59ae2cfff2aadb1869423e
geofighter <itmarcosgeo@gmail.com>
2022-10-17 16:11:25 -0500

    Add a spinner loader, from material ui

---
+++ src/ui/components/CheckingAuth.jsx
@@ -0,0 +1,22 @@
+import {CircularProgress, Grid} from "@mui/material";
+
+export const CheckingAuth = () => {
+
+    return (
+            <Grid container
+                  spacing={ 0 }
+                  direction="column"
+                  alignItems="center"
+                  sx={{ minHeight: '100vh', backgroundColor: 'secondary.aux', padding: 4, paddingTop: 20 }}
+            >
+                <Grid
+                    item
+                    direction="row"
+                    justifyContent="center"
+                >
+                    <CircularProgress color='primary' />
+                </Grid>
+            </Grid>
+    );
+
+}
