401465045f82d49bd24069f8bc1d1a798927042b
geofighter <73375312+geofighter@users.noreply.github.com>
2022-10-27 11:47:45 -0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,6 @@ import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-/*
-
-    NOTE: ONCE THAT FILL THE FIELDS "YourCredentials" with your data,
-    RENAME THIS FILE AS "config.js" to connect with firebase services
-
-*/
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "YourCredentials",
